4701b6ea1a440398a6fa0198174963d512d3d1d6
Cluley <oo43@yandex.ru>
2023-04-28 16:49:54 +0700

    initial commit

---
+++ variant.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <variant>
+#include <vector>
+#include <any>
+
+std::variant<int, std::string, std::vector<int>> get_variant() {
+	std::srand(std::time(nullptr));
+	int random_variable = std::rand() % 3;
+
+	std::variant<int, std::string, std::vector<int>> result;
+	switch (random_variable)
+	{
+	case 0:
+		result = 5;
+		break;
+	case 1:
+		result = "Hello World!";
+		break;
+	case 2:
+		result = std::vector<int>{ 1, 2, 3, 4, 5 };
+		break;
+	default:
+		break;
+	}
+	return result;
+}
+
+int main()
+{
+	auto tmp = get_variant();
+
+	if (std::holds_alternative<int>(tmp)) {
+		std::cout << (std::get<int>(tmp) * 2) << std::endl;
+	}
+	else if (std::holds_alternative<std::string>(tmp)) {
+		std::cout << std::get<std::string>(tmp) << std::endl;
+	}
+	else if (std::holds_alternative<std::vector<int>>(tmp)) {
+		for (const int& n : std::get<std::vector<int>>(tmp)) {
+			std::cout << n << ' ';
+		}
+	}
+
+	return 0;
+}
